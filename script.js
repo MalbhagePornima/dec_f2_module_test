2d5b89f92350bd7c659105cdc6b884db2da03036
Pornima_Malbhage <malbhage.pornima15@gmail.com>
2023-12-03 23:26:44 +0530

    My Project

---
+++ script.js
@@ -0,0 +1,62 @@
+const activeTimers=document.getElementById("active-timers");
+
+function startNewTimers(){
+    const hours=document.getElementById("hours").value || 0;
+    const minutes=document.getElementById("minutes").value || 0;
+    const seconds=document.getElementById("seconds").value || 0;
+
+    if(!hours && !minutes && ! seconds){
+        alert("please enter a valid time");
+        return;
+    }
+
+    const total_time_in_seconds=parseInt(hours) * 3600 + parseInt(minutes)* 60 + parseInt(seconds);
+
+    createTimer(total_time_in_seconds);
+}
+
+function createTimer(time_in_seconds){
+    const timerDiv=document.createElement("div");
+
+    const timerDisplay=document.createElement("span");
+
+    timerDiv.appendChild(timerDisplay);
+    const timeInterval=setInterval(()=>{
+        time_in_seconds --;
+
+        const display_hours=Math.floor(time_in_seconds/3600);
+        const display_minutes=Math.floor((time_in_seconds%3600)/60);
+        const display_seconds=Math.floor(time_in_seconds%60);
+
+        timerDisplay.textContent ='Time Left: ${display_hours}:${display_minutes}:${display_seconds}';
+        console.log("workinng");
+    
+        if(time_in_seconds<=0){
+            clearInterval(timeInterval);
+
+            const end_msg=document.createElement("div");
+            end_msg.innerText="Timer is up !";
+            timerDiv.innerHTML="";
+            timerDiv.appendChild(end_msg);
+
+        }
+    },1000);
+    activeTimers.appendChild(timerDiv);
+
+    const stopButton=document.createElement("button");
+    stopButton.textContent="stop";
+    stopButton.onclick=()=>clearInterval(timeInterval);
+
+    const deleteButton=document.createElement("button");
+    deleteButton.textContent="Delete";
+    deleteButton.onclick=()=>{
+        clearInterval(timeInterval);
+        timerDiv.remove();
+    };
+
+    activeTimers.appendChild(timerDiv);
+    timerDiv.appendChild(stopButton);
+    timerDiv.appendChild(deleteButton);
+
+
+}
